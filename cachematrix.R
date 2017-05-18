a7c7dcd5b7e413986f64806b7f6d432f0a5b1687
David Johns <beagledog@users.noreply.github.com>
2017-05-18 17:33:08 -0400

    Adding code for peer review.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is Assignment 2 of the R Programming course
+## for the Johns Hopkins Data Science Specialization.
+## The main purpose of the two functions makeCacheMatrix and 
+## cacheSolve, is to understand lexical scoping.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object 
+## that can cache its inverse.
+## The function begins with an empty matrix, 'x'.
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL 
+  set <- function(y) {
+      x <<- y
+      inv <<- NULL
+  }
+  
+## Define getter for matrix 'x'
+  get <- function() x
 
+## Define setter for inverse 'inv'
+  setSolve <- function(solve) inv <<- solve
+
+## Define getter for inverse 'inv'
+  getSolve <- function() inv
+  
+## Define the list of named elements, the nested functions.
+  list(set = set, get = get,
+       setSolve = setSolve,
+       getSolve = getSolve)
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix above. If the inverse has already 
+## been calculated (and the matrix has not changed), then the cacheSolve 
+## should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  ## Check to see if the cache is not null
+  inv <- x$getSolve()
+  if(!is.null(inv)) {
+    ## If cache is not null, return cache value 'inv'
+    message("getting cached inverse")
+    return(inv)
+  }
+  ## If cache is null, solve new inverse 'inv_new'
+  data <- x$get()
+  inv_new <- solve(data, ...)
+  x$setSolve(inv_new)
+  inv_new
 }
